b25d4ffdec25e560213c281c6ef39754d68eefa1
belen <belentorreshuanque@gmail.com>
2023-10-20 09:12:46 -0500

    v6. Instalo fb: auth, storage, firestore

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
     <div style={{background:"yellow", width:"350px", padding:"10px"}}>
       <Appform/>
       <i class="large material-icons">insert_chart</i>
+
+      <p>1. Jose Manuel 28 Masculino ---- x - A</p>
+      <p>2. Shirley Beatriz 32 Masculino ---- x - A</p>
+      <p>3. Edgar Junior 24 Masculino ---- x - A</p>
     </div>
   );
 }
